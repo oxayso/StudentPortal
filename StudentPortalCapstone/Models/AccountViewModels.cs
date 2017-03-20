3d8cda450d93f3bc9554ee5f84547155635d8228
Olivia Xaysombath <Olivia Xaysombath>
2017-03-20 11:45:06 -0500

    installed ckeditor, need to add additional plugin for file uploading

--- StudentPortalCapstone/Models/AccountViewModels.cs
+++ StudentPortalCapstone/Models/AccountViewModels.cs
@@ -88,7 +88,7 @@ namespace StudentPortalCapstone.Models
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
+        [Display(Name = "Confirm Password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
